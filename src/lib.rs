81c6ad2f33aafcbc990bc00429b789f77e5e3632
Keith T. Star <keith@uberfoo.com>
2019-04-27 07:48:08 -0700

    Update Documentation
    I lost track of how this works, so I started adding some documentation.
    
    The README.md file is meant to be generated: `cargo readme > README.md`.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,60 @@
 #![warn(missing_docs)]
-//! Another file system: UberFS
+//! A different sort of file system: UberFS
+//!
+//! # FIXME
+//! I need to touch on the following topics:
+//!  * Distributed block storage
+//!  * Block level encryption
+//!  * Merkle Tree usage
+//!  * WASM support
+//!
+//! # File System Structure
+//!
+//! Like every other file system, with some possible few exceptions, fixed-size blocks are the
+//! foundation of UberFS. What's different across all file systems is how they are utilized.
+//! Particularly, in how the file system metadata is represented.  We'll touch on blocks, and other
+//! miscellany, but the primary focus of this section will be metadata representation.
+//!
+//! ## Metadata
+//!
+//! All metadata is stored in a dictionary. The dictionary is serialized with to a `Vec<u8>` with
+//! `Serde` and `Bincode`, and written to blocks. If the serialized dictionary does not fit within a
+//! single block, it will contain a pointer to the next block.  The pointer is stored under the key
+//! `@next_block`. Additionally, eac block is identified by a `@type` key, where the value may be
+//! something like `directory`, `fs-metadata`, etc.  Additional metadata for each specific block
+//! type may exist under the `@metadata` key.  Data specific to the block type lives under the
+//! `@data` key. Finally, each dictionary contains a `@checksum` key that contains the checksum
+//! for the entire dictionary.
+//!
+//! ### Block Trees
+//!
+//!
+//! ## Blocks
+//!
+//! The blocks are fixed-size, and uniform for a 044iven file system.  Blocks contain nothing more
+//! than the raw data that is written to them.  Put another way, there is no metadata _about_ the
+//! block stored in the block _itself_.  Rather, metadata blocks contain information about blocks
+//! that make up files and directories.
+//!
+//! ### Block 0
+//!
+//! The first block is the file system is special.  It contains information about the file system
+//! itself, such as the number of blocks, the block size, a free block list, etc.
+//!
+//! The following is currently how block 0 is organized; it's serialized to a `Vec<u8>` using
+//! `Serde` and `Bincode`:
+//!
+//! ```
+//! pub(crate) struct BlockMetadata {
+//!    pub size: BlockSize,
+//!    pub count: BlockCardinality,
+//!    pub next_free_block: Option<BlockCardinality>,
+//!    pub directory: HashMap<String, Block>,
+//!}
+//! ```
+//!
+//! Note that the above flies in the face of what was described above -- this is clearly not a
+//! dictionary.  Instead, it's legacy code that needs to be updated.
 //!
 use std::{cell::RefCell, path::Path, rc::Rc};
 
@@ -13,13 +68,15 @@ pub mod io;
 // mod vm;
 
 pub(crate) use {
-    block::{BlockCardinality, BlockSize, BlockType},
+    block::{BlockSize, BlockType},
     directory::{Directory, DirectoryEntryReader, DirectoryEntryWriter, MutableDirectory},
 };
 
 pub use block::{
+    load_file_store,
     manager::BlockManager,
     storage::{file::FileStore, BlockStorage},
+    BlockCardinality,
 };
 
 use crate::io::{BlockTreeReader, BlockTreeWriter};
