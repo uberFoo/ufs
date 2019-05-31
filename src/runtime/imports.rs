13158bb564da8ea1897b62bdd2ec6523051d941f
Keith T. Star <keith@uberfoo.com>
2019-05-31 08:20:42 -0700

    Minor, tiny movement on wasm front.

--- src/runtime/imports.rs
+++ src/runtime/imports.rs
@@ -10,10 +10,47 @@ use wasmi::{
 /// built atop these.
 ///
 /// List files in a directory
-pub const LIST_FILES_INDEX: usize = 0;
+const LIST_FILES_NAME: &'static str = "list";
+const LIST_FILES_INDEX: usize = 0;
 /// Open a file
-pub const OPEN_FILE_INDEX: usize = 1;
+const OPEN_FILE_NAME: &'static str = "open";
+const OPEN_FILE_INDEX: usize = 1;
 /// Read from a file
-pub const READ_FILE_INDEX: usize = 2;
+const READ_FILE_NAME: &'static str = "read";
+const READ_FILE_INDEX: usize = 2;
 /// Write to a file
-pub const WRITE_FILE_INDEX: usize = 3;
+const WRITE_FILE_NAME: &'static str = "write";
+const WRITE_FILE_INDEX: usize = 3;
+
+pub struct RuntimeModuleImportResolver;
+
+impl RuntimeModuleImportResolver {
+    pub fn new() -> RuntimeModuleImportResolver {
+        RuntimeModuleImportResolver {}
+    }
+}
+
+impl<'a> ModuleImportResolver for RuntimeModuleImportResolver {
+    fn resolve_func(
+        &self,
+        field_name: &str,
+        _signature: &Signature,
+    ) -> Result<FuncRef, InterpreterError> {
+        let func_ref = match field_name {
+            LIST_FILES_NAME => {
+                FuncInstance::alloc_host(Signature::new(&[][..], None), LIST_FILES_INDEX)
+            }
+            OPEN_FILE_NAME => FuncInstance::alloc_host(
+                Signature::new(&[ValueType::I32][..], None),
+                OPEN_FILE_INDEX,
+            ),
+            _ => {
+                return Err(InterpreterError::Function(format!(
+                    "no function exported with name {}",
+                    field_name
+                )))
+            }
+        };
+        Ok(func_ref)
+    }
+}
